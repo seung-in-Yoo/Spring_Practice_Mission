4e0020a389059d8bd3e736005e5996aae115ad98
seungin <tmddls91@inha.edu>
2025-06-30 04:07:09 +0900

    [test] ci cd 테스트

--- src/test/java/umc/UMC8th/Umc8thApplicationTests.java
+++ src/test/java/umc/UMC8th/Umc8thApplicationTests.java
@@ -2,8 +2,10 @@ package umc.UMC8th;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Umc8thApplicationTests {
 
 	@Test
